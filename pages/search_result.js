dba9a68e5bd63ae1e573947f26475452d755c447
mitranikhil44 <mitranikhil93@gmail.com>
2023-10-02 00:05:01 +0530

    Initial commit

--- pages/search_result.js
+++ pages/search_result.js
@@ -38,16 +38,23 @@ const Search_Result = () => {
         loader={<h4>Loading...</h4>}
       >
         <div className="grid grid-cols-2 mt-4 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 overflow-hidden">
-          {items.map((element) => (
+          {items.map((element) => {
+            const imdbRating = parseFloat(element.imdb?.[0]?.match(/[\d.]+/) || '0');
+            return(
             <Link key={element.slug} href={`/${element.slug}`}>
               <div className="to-black relative overflow-hidden rounded-lg shadow-lg hover:scale-105 cursor-pointer transition-transform duration-300 ease-in-out h-[100%]">
                 {element.image ? (
-                  <div className="w-full overflow-hidden flex items-center justify-center cropped-image">
+                  <div className="w-full overflow-hidden flex items-center justify-center">
+                    <div className="relative">
                     <Image width="140" height="140"
                       src={element.image}
                       alt="Image"
                       className="object-cover w-full"
                     />
+                    <p className={`z-10 IMDB rounded-tl-lg absolute top-0 left-0 overflow-hidden p-[1%] text-white ${imdbRating >= 9 ? 'bg-green-700' : imdbRating >= 8 ? "bg-green-600": imdbRating >= 7 ? "bg-green-500": imdbRating >= 6.5 ? "bg-yellow-700": imdbRating >= 6 ? "bg-yellow-600": imdbRating >= 5.5 ? "bg-yellow-500": imdbRating >= 5 ? "bg-red-500": imdbRating >= 4.5 ? "bg-red-600": "bg-red-700"}`}>
+                {imdbRating.toFixed(1)}
+              </p>
+                    </div>
                   </div>
                 ) : (
                   // Handle missing image or provide a placeholder
@@ -60,7 +67,7 @@ const Search_Result = () => {
                 </div>
               </div>
             </Link>
-          ))}
+          )})}
         </div>
       </InfiniteScroll>
     </>
